fe73081570c92ea0634f31576108293e615c20b8
Nico <redirectn1k@gmail.com>
2022-02-22 13:43:26 +0100

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Easy Classics',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'AlphaFitness KFA Rechner'),
+      home: const MyHomePage(title: 'Easy Classics'),
     );
   }
 }
